fb04ede99aeac118e4ade1e8f9aaeef598fa48a3
harpreet1025 <harpreet@chatimity.com>
2014-12-02 21:54:47 +0530

    Initial running code in python3 and aiohttp lib

--- basics.py
+++ basics.py
@@ -1,12 +1,16 @@
+# pip3 install aiohttp for getting aiohttp library
+
 import asyncio
 import aiohttp
 
 @asyncio.coroutine
 def print_page(url):
     response = yield from aiohttp.request('GET', url)
-    body = yield from response.read_and_close(decode=True)
+    # read_and_close function is deprecated
+    # read(True) is deprecated, use .json instead
+    body = yield from response.read()
     print(body)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(print_page('http://www.google.com'))
+loop.run_until_complete(print_page('http://www.chatimity.com'))
